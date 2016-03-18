410acc1e019f284fce5f244e1846cb6f2bc91975
Jairo <kidandcat@gmail.com>
2016-03-18 13:14:30 +0100

    delete with password now

--- api/pdf.js
+++ api/pdf.js
@@ -104,7 +104,7 @@ router.post('/pdf/:path', multipartMiddleware, function(req, res, next) {
     });
 });
 
-router.delete('/pdf/:path/:id', function(req, res, next) {
+router.delete('/pdf/:path/:id/:password', function(req, res, next) {
     if (req.params.password == passwords[req.params.path + encodeURIComponent(req.params.id)]) {
         deleteFile('pdfs/' + req.params.path + '/' + req.params.id, function(result) {
             res.send(result);
